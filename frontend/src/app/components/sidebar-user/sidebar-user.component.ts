65328a23ba8a9fda4fef859ff3f94373ea964c7a
fadal ba <fadalba@gmail.com>
2023-01-07 01:51:01 +0000

    +

---
+++ frontend/src/app/components/sidebar-user/sidebar-user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-user',
+  templateUrl: './sidebar-user.component.html',
+  styleUrls: ['./sidebar-user.component.scss']
+})
+export class SidebarUserComponent {
+
+}
