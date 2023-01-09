ecce727ac4e73b4bf38b062492f2ccb279310a6e
kant9 <cd946@gmail.com>
2023-01-09 11:41:31 +0100

    Ajout des components sidebars, connexion et inscription

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { ConnectionComponent } from './components/connection/connection.component';
 import { InscriptionComponent } from './components/inscription/inscription.component';
 import { PageAdminComponent } from './components/page-admin/page-admin.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AuthGuard } from "./service/auth.guard";
 
 const routes: Routes = [
   { path: '', redirectTo: '/log-in', pathMatch: 'full' },
-  { path: 'log-in', component: ConnectionComponent },
+  { path: 'log-in', component: SidebarComponent },
   { path: 'sign-up', component: InscriptionComponent },
   { path: 'user-profile/:id', component: PageAdminComponent, canActivate: [AuthGuard] }
 ];
