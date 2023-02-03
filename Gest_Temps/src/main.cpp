9b8b505c958b946a8a2d6bc36e30cfb06b9da7cc
Siaka <isacadel98@gmail.com>
2023-02-03 10:11:38 +0000

    12134

--- Gest_Temps/src/main.cpp
+++ Gest_Temps/src/main.cpp
@@ -5,78 +5,127 @@
 #include "DHT.h"   
 #include<IRremote.h>  
 
-
-#define LedMarcheV 7
-#define LedArreeR 8
-
 #define DHTPIN 2   
 #define DHTTYPE  DHT11
 
-
-
 #define Ventilo 5
-const char DIN_RECEPTEUR_INFRAROUGE = 10;
+
+const int PIN_RECEPTEUR_INFRAROUGE = 10;
 #define Buzzer 9
 
 DHT dht(DHTPIN, DHTTYPE);
 
 
-IRrecv monRecepteurInfraRouge (DIN_RECEPTEUR_INFRAROUGE);
+IRrecv monRecepteurInfraRouge(PIN_RECEPTEUR_INFRAROUGE);
 
 decode_results messageRecu;
 
 
 void setup() {   
-    pinMode(LedMarcheV,OUTPUT);
-    pinMode(LedArreeR,OUTPUT);
+
+    pinMode(Ventilo,OUTPUT);
     monRecepteurInfraRouge.enableIRIn();
     monRecepteurInfraRouge.blink13(true);
     Serial.begin(9600);    
     dht.begin(); 
     delay(100);
 }
+int InterfaceButun = LOW;
+int maCommande = LOW;
+int maCommande0 = LOW;
 void loop() 
 {   
+    char incomingByte = Serial.read();
     float humidite = dht.readHumidity();
     float temperature = dht.readTemperature();
-
-      if(monRecepteurInfraRouge.decode(&messageRecu))
+if(temperature >30)
       {
-          if (messageRecu.value == 0xFF30CF)
-        {
-          digitalWrite(LedMarcheV,1);
-          digitalWrite(LedArreeR,0);
-          delay(10000);
-        }
-            if (messageRecu.value == 0xFF18E7)
-        {
-          digitalWrite(LedMarcheV,0);
-          digitalWrite(LedArreeR,1);
-          delay(10000);
-        }
-      }
-
-    if( temperature >30)
-      {
+            digitalWrite(Ventilo,HIGH);
             tone(Buzzer,1000,500);
-            digitalWrite(Ventilo,1);
-            digitalWrite(LedMarcheV,1);
-            digitalWrite(LedArreeR,0);
       }
-      else
-      {
-        noTone(Buzzer);
-        digitalWrite(Ventilo,0);
-        digitalWrite(LedMarcheV,0);
-        digitalWrite(LedArreeR,1);
-      }
+    
+   if (monRecepteurInfraRouge.decode(&messageRecu))
+  {
+    
+    if (messageRecu.value == 0xFF30CF)
+        {
+          maCommande = HIGH;
+          if (maCommande == HIGH)
+          {
+            digitalWrite(Ventilo,HIGH);
+          delay(1000);
+          }
+          
+        }
+        else if (messageRecu.value == 0xFF6897)
+          { maCommande = LOW;
+            if (maCommande == LOW)
+            {
+              digitalWrite(Ventilo,LOW);
+              delay(1000);
+            }
+            
+          }
+      monRecepteurInfraRouge.resume();
+  }
   
+      /*Serial.println(incomingByte);*/
       
+    
+     
+     
+    if(incomingByte == '1')
+    {
+      InterfaceButun =HIGH;
+      if (InterfaceButun == HIGH)
+      {
+        digitalWrite(Ventilo,HIGH);
+      }
+    }
+    else if(incomingByte == '0')
+    {
+      InterfaceButun =LOW;
+      if (InterfaceButun == LOW)
+      {
+        if(digitalRead(Ventilo == HIGH))
+        {
+          digitalWrite(Ventilo,LOW);
+        }
+      }
+    }
+    
+       else 
+      {
+        if (maCommande == HIGH || InterfaceButun == HIGH || temperature >30)
+        {
+          digitalWrite(Ventilo,HIGH);
+        }
+        else if (maCommande == LOW || InterfaceButun == LOW ||temperature<30)
+        {
+          maCommande0 = LOW;
+          if (maCommande0 == LOW)
+          {
+            digitalWrite(Ventilo,LOW);
+          }
+          
+        }
+        //noTone(Buzzer);
+        //digitalWrite(Ventilo,LOW);
+        }
+     
+       
+     
+
    
-    Serial.print(dht.readTemperature());  
-    Serial.print(" C"); 
+  // Serial.println(Serial.read());
+  
+
+   
+    Serial.print(temperature); 
+    delay(800); 
+    Serial.print("°C"); 
     Serial.print("/");   
-    Serial.print(dht.readHumidity());   
+    Serial.print(humidite);   
     Serial.println(" %"); 
-    delay(200); 
+    delay(500); 
 }
